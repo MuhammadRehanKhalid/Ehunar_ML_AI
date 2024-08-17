7fda99149a5e05aa85fa2c64ba965d1d114f75be
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-08-17 21:02:13 +0000

    Modules_study

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -8,4 +8,16 @@
 
 
 # How does it works
+# body
+from america import body
+# chasis
+from china import chasis
+# engines
+from germany import engines
+# tyres
+from italy import tyres
 
+def car_ass()
+print(f'{body()},{none},{none}, and {none} makes a car')
+
+car_ass()
---
+++ china.py
@@ -0,0 +1,2 @@
+def chasis():
+    return chasis
