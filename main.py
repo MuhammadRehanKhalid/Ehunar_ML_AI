744f26e308f7c076f1a82966e570cdb6e9d189e4
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-08-18 02:16:07 +0500

    Module_sustained

--- main.py
+++ main.py
@@ -9,15 +9,17 @@
 
 # How does it works
 # body
-from america import body
-# chasis
-from china import chasis
-# engines
-from germany import engines
-# tyres
-from italy import tyres
+# from West.america import body
+# # chasis
+# from East.china import chasis
+# # engines
+# from West.germany import engines
+# # tyres
+# from West.italy import tyres
 
+from West import america, italy, germany
+from East import china
 def car_ass():
-    print(f'{body()},{chasis()},{engines()}, and {tyres()} makes a car')
+    print(f'{america.body()},{china.chasis()},{germany.engines()}, and {italy.tyres()} makes a car')
 
 car_ass()
