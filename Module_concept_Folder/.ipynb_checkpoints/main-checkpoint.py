c9206537e74ad0427d8a6cdfe8cdcc305f8b735b
Muhammad Rehan Khalid <m.rehankhalid@gcuf.edu.pk>
2024-08-18 13:38:14 +0000

    Folder_Summarized

---
+++ Module_concept_Folder/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,23 @@
+# Module
+
+
+# what is module
+
+
+# why we need it
+
+
+# How does it works
+# body
+from america import body
+# chasis
+from china import chasis
+# engines
+from germany import engines
+# tyres
+from italy import tyres
+
+def car_ass()
+print(f'{body()},{none},{none}, and {none} makes a car')
+
+car_ass()
